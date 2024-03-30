22a21cc093e15704150b4622b40dcc8249b22d17
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-30 10:50:43 -0600

    feat: create Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: "1fr",
+        lg: "250px 1fr",
+      }}
+    >
+      <GridItem bg="orange.300" area={"nav"}>
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" area={"aside"} paddingX={5}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
