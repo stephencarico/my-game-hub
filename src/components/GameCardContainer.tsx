43500a4ed8800c84be2d3c6309ef86d73e523b97
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-03-31 18:42:06 -0600

    feat: GameCardContainer

---
+++ src/components/GameCardContainer.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from "react";
+import { Box } from "@chakra-ui/react";
+
+const GameCardContainer = ({ children }: { children: ReactElement }) => {
+  return (
+    <Box borderRadius={10} overflow="hidden">
+      {children}
+    </Box>
+  );
+};
+
+export default GameCardContainer;
--- src/components/GamesGrid.tsx
+++ src/components/GamesGrid.tsx
@@ -1,21 +1,28 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, SimpleGrid, GridItem } from "@chakra-ui/react";
 
 import useGames from "../hooks/useGames";
 
 import GameCard from "./GameCard";
+import GameCardContainer from "./GameCardContainer";
 
 const GamesGrid = () => {
   const { data } = useGames();
 
   return (
     <Box p={2}>
-      <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        padding="10px"
+        spacing={6}
+      >
         {data.map((game) => (
           <GridItem key={game.id}>
-            <GameCard game={game} />
+            <GameCardContainer>
+              <GameCard game={game} />
+            </GameCardContainer>
           </GridItem>
         ))}
-      </Grid>
+      </SimpleGrid>
     </Box>
   );
 };
