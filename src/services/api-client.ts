06592ff3239332ee05beff0698126a1738e34f27
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-06 13:09:35 -0600

    chore: update api key

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: '6face79c37a94a9f9ede3266b4b7ff7e'
+    key: '319d499f464e431b859016d9af7835b2'
   }
 })
