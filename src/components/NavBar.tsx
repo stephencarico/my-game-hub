2b0857f94b358f291b80f09de5b7e1bcfbbcc826
Stephen Carico <32233894+stephencarico@users.noreply.github.com>
2024-04-06 14:04:55 -0600

    feat: user can search

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,11 +4,15 @@ import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import logo from "../assets/logo.webp";
 
-const NavBar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const NavBar = ({ onSearch }: Props) => {
   return (
     <HStack p={"10px"}>
       <Image w="60px" h="60px" src={logo} objectFit="contain" alt="App Logo" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,14 +1,27 @@
+import { useRef } from "react";
 import { Input, InputLeftElement, InputGroup } from "@chakra-ui/react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref?.current) onSearch(ref.current?.value);
+      }}
+    >
       <InputGroup w="100%">
         <InputLeftElement pointerEvents="none">
           <BsSearch />
         </InputLeftElement>
         <Input
+          ref={ref}
           borderRadius={20}
           placeholder="Search games..."
           variant="filled"
